3ce3fe6899d6d095a230a58f7d6481385c743d5a
Karllos <138056516+karlloscosta@users.noreply.github.com>
2023-07-30 22:00:07 -0300

    Update questao_1.c

--- Unit_I/Ponteiros/questao_1.c
+++ Unit_I/Ponteiros/questao_1.c
@@ -4,12 +4,12 @@ int main(void){
     
     int x, y, *ponteiro;
     y = 0;
-    ponteiro = &y;
-    x = *ponteiro;
-    x = 4;
-    (*ponteiro)++;
+    ponteiro = &y; //atribuindo o endereço de y ao ponteiro
+    x = *ponteiro; //atribuindo o valor que o ponteiro guarda a x
+    x = 4; 
+    (*ponteiro)++;//somando + 1 ao conteudo do ponteiro
     --x;
-    (*ponteiro) += x;
+    (*ponteiro) += x;// somando x com o ponteiro,
 
     printf("ponteiro = %p, x = %d, y = %d", ponteiro, x, y);
     
