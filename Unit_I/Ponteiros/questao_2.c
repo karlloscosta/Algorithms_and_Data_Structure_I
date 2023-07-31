75eb79dc03af6d5f0deccb2745dd0500df1ae3dd
Karllos <138056516+karlloscosta@users.noreply.github.com>
2023-07-30 22:04:50 -0300

    Update questao_2.c

--- Unit_I/Ponteiros/questao_2.c
+++ Unit_I/Ponteiros/questao_2.c
@@ -3,20 +3,21 @@
 int main(void) {
    
     int a, b, c, d;
-    int *p1;
+    int *p1; //declaração do ponteiro p1
+   //atribuindo endereços aos ponteiros
     int *p2 = &a;
     int *p3 = &c;
-    p1 = p2;
-    *p2 = 10;
+    p1 = p2; //passando o endereço de p2 para p1
+    *p2 = 10; //o conteudo de a é 10
     b = 20;
-    int **pp;
-    pp = &p1;
-    *p3 = **pp;
-    int *p4 = &d;
+    int **pp;//ponteiro de ponteiro
+    pp = &p1; //passando o endereço de p1
+    *p3 = **pp;//c passa ser igual a *p1
+    int *p4 = &d;// mais uma declaração de ponteiro
     *p4 = b + (*p1)++;
     printf("%d\t%d\t%d\t%d\n", a, b, c, d);
     
     return 0;
 }
 
-    // Os resultados serão a = 11, b = 20, c = 10 e d = 30
+    // Os resultados serão a = 11, b = 20, c = 10 e d = 31
