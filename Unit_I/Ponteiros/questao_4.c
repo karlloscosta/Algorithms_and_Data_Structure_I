b3bd7b3778091c8188c0029f9d78d04547b7576c
Karllos <138056516+karlloscosta@users.noreply.github.com>
2023-07-30 22:09:16 -0300

    Update questao_4.c

--- Unit_I/Ponteiros/questao_4.c
+++ Unit_I/Ponteiros/questao_4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
-
+//função que calcula
 void hexagono(float l, float *area, float *perimetro){
-
+   //uso de ponteiros para modificar as variaveis fora da função
     *area = (3*pow(l, 2)*sqrt(3))/2;
     *perimetro = 6*l;
 
@@ -15,8 +15,8 @@ int main(void){
     printf("Digite o valor do lado (l) do hexagono:\n");
     scanf("%f", &l);
 
-    calcula_hexagono(l, &area, &perimetro);
-    printf("A area do hexagono e: %f, e o perimetro e: %f", area, perimetro);
+    calcula_hexagono(l, &area, &perimetro);//chamada da função
+    printf("A area do hexagono e: %f, e o perimetro e: %f", area, perimetro);// passagem com os valores alterados
 
     return 0;
 }
