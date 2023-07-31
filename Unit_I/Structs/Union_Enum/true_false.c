a3c50f571ff25004cc14e15f10c338d23f10016c
Karllos <138056516+karlloscosta@users.noreply.github.com>
2023-07-30 22:19:57 -0300

    Update true_false.c

--- Unit_I/Structs/Union_Enum/true_false.c
+++ Unit_I/Structs/Union_Enum/true_false.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-
+// definindo constantes
 #define TRUE 1
 #define FALSE 0
-
+//criando uma enum
 typedef enum bool{
     false, true
 }Bool;
 
 int main (void){
-
+    //declarando variavel
     Bool val;
 
     printf("Digite 0 ou 1\n");
     scanf("%d", &val);
-
+    //teste e atribuição
     printf("Valor informado: %d\n", val);
     if(val==true){
         printf("Verdadeiro \n");
